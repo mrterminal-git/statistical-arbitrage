8a53d4c9a1a5d49120eda1f3a53be6dce92dc474
Volter Entoma <volterentoma1@gmail.com>
2024-11-24 15:09:52 +1000

    style: delete test code and edit for example run on new function

--- src/main.cpp
+++ src/main.cpp
@@ -24,96 +24,21 @@ const std::string& START_DATE = "2014-11-23", END_DATE = "2024-11-23";
 // Define the price type
 const std::string& PRICE_TYPE = "adj close";
 
-// Loading base stock
-Stock stock2;
-const std::string& stock2Symbol = "SPY";
-FileReader::loadStockDataFromFile(Config::getStockDataDir() + stock2Symbol + TEXT_EXTENSION, stock2);
-std::unordered_map<std::string, double> stock2Data = StockUtils::getPriceDataInRange(stock2, PRICE_TYPE, START_DATE, END_DATE);
-std::unordered_map<std::string, double> stock2NormalizedData = StockAnalysis::normalizeToEarliestDate(stock2Data);
+// Define benchmark stock
+const std::string& BENCHMARK_STOCK = "SPY";
 
+// Compare against function version and output
+auto functionOutput = PairsTradingBackTesting::selectPairsForBackTesting(
+	nyseListings,
+	Config::getStockDataDir(),
+	TEXT_EXTENSION,
+	BENCHMARK_STOCK,
+	PRICE_TYPE,
+	START_DATE,
+	END_DATE
+);	
 
-struct LNpairStatistic{
-	std::string pair;
-	std::string pairDateRange;
-	double slope;
-	double slopeError;
-	int df;
-	double testStatistic;
-	double pValue;
-	double rSquared;
-	double lastDifference;
-};
-
-// Initialize map to store results
-std::unordered_map<std::string, LNpairStatistic> pairResults;
- 
-for(const std::string& nyseListing : nyseListings) {
-	// Loading comparison data
-	Stock stock1;
-	
-	const std::string& stock1Symbol = nyseListing;
-
-	std::cout << "-----------------------------\n" << stock1Symbol << "-" << stock2Symbol  << "\n";
-	
-	// Loading the data into stock object
-	FileReader::loadStockDataFromFile(Config::getStockDataDir() + stock1Symbol + TEXT_EXTENSION, stock1);
-	
-	// Get the specific price type and date range data
-	std::unordered_map<std::string, double> stock1Data = StockUtils::getPriceDataInRange(stock1, PRICE_TYPE, START_DATE, END_DATE);
-
-	// Normalize the data
-	std::unordered_map<std::string, double> stock1NormalizedData;
-	try{
-		stock1NormalizedData = StockAnalysis::normalizeToEarliestDate(stock1Data);
-	
-	} catch (std::exception& e){
-		std::cout << e.what() << "\n";
-		continue;
-		
-	}
-
-	// Get the difference
-	std::unordered_map<std::string, double> differenceOfNormalized = StockAnalysis::calculateDifferenceBetweenData(stock1NormalizedData, stock2NormalizedData);
-
-	// Get the sorted version
-	std::vector<std::pair<std::string, double>> sortedDifferenceOfNormalzied = StockAnalysis::sortMap(differenceOfNormalized);
-
-	// Get the LN(difference)
-	std::unordered_map<std::string, double> LnDifferenceOfNormalized 
-		= StockAnalysis::applyMapOperation(differenceOfNormalized, [](double value) { return std::log(value); });
-
-	// Perform linear regression get linear fit
-	StatisticalAnalysis::LinearRegressionResult stats = StatisticalAnalysis::linearRegression(LnDifferenceOfNormalized);
-
-	// Get the t-student p-value of the linear fit
-	double linearFitPvalue;
-	try {
-		linearFitPvalue = StatisticalAnalysis::calculatePValue(
-		stats.slope / stats.slopeError,
-		LnDifferenceOfNormalized.size(),
-		"t");
-	} catch (std::exception& e) {
-		std::cout << e.what() << "\n";
-		continue;
-	}
-
-	LNpairStatistic currentPairStatistics = {
-		stock1Symbol + "-" + stock2Symbol,
-		START_DATE + "-" + END_DATE,
-		stats.slope, 
-		stats.slopeError,
-		LnDifferenceOfNormalized.size(),
-		stats.slope / stats.slopeError,
-		linearFitPvalue,
-		stats.rSquared,
-		sortedDifferenceOfNormalzied.back().second
-	};
-	
-	pairResults[stock1Symbol + "-" + stock2Symbol] = currentPairStatistics;
-}
-
-// Output all results into a file
-std::ofstream outFile(Config::getOutputDir() + "LN_Difference_Pairs.txt");
+std::ofstream outFile(Config::getOutputDir() + "LN_Difference_Pairs_2.txt");
 outFile << "Pair,Date Range,Slope,Slope Error,Test Statistic,DF,p-value,R-squared,last difference\n";
 for(auto element : functionOutput) {
 	outFile << 	
